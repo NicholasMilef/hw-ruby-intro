05e5b2f9cf9f63ba9284a5583fe2e2ac7a962883
EC2 Default User <ec2-user@ip-172-31-77-190.ec2.internal>
2021-09-12 02:31:17 +0000

    Finished exercise 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,38 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  arr.each do |x| sum = sum + x  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if (arr.length == 0) then
+    return 0
+  elsif (arr.length == 1) then
+    return arr[0]
+  end
+
+  sorted_arr = arr.sort()
+
+  return sorted_arr[-1] + sorted_arr[-2]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sorted_arr = arr.sort()
+  l = 0
+  r = arr.length - 1
+  while l < r do
+    sum = sorted_arr[l] + sorted_arr[r]
+    if sum == n then
+      return true
+    elsif sum < n then
+      l += 1
+    else
+      r -= 1
+    end
+  end
+  return false
 end
 
 # Part 2
